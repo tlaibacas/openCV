ec31df99672b310491260b686ff586bec9566f51
tlaibacas <tiago.abreu.laibacas@gmail.com>
2026-05-01 18:44:24 +0100

    updated versiona dn user schema

--- users.sql
+++ users.sql
@@ -1,12 +1,29 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    name TEXT NULL,
-    last_name TEXT NULL,
-    sex TEXT NULL,
+
+    name TEXT,
+    last_name TEXT,
+
+    sex TEXT DEFAULT 'NAN',
+    CHECK (sex IN ('male', 'female', 'other', 'NAN')),
+
     role TEXT NOT NULL DEFAULT 'visitor',
     CHECK (role IN ('admin', 'agent', 'visitor')),
-    agency TEXT NULL,
-    created_at TIMESTAMPTZ DEFAULT NOW()
+
+    agency TEXT,
+
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+
+    CHECK (
+        (role = 'visitor' AND name IS NOT NULL)
+        OR
+        (role = 'agent' AND agency IS NOT NULL)
+        OR
+        (role = 'admin')
+    )
 );
