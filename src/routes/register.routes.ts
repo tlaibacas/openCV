0ac5f9d850b0139765b7fb41c93de4d031d5fb31
tlaibacas <tiago.abreu.laibacas@gmail.com>
2026-05-01 17:02:20 +0100

    routes with functions

--- src/routes/register.routes.ts
+++ src/routes/register.routes.ts
@@ -1,25 +1,31 @@
 import type { FastifyInstance } from "fastify";
 import { query } from "../db/index.js";
-
 import { bruteShield } from "../middleware/bruteShield.js";
+import { registerUser } from "../auth/register/register.queries.js";
 
 export async function rootRoutes(fastify: FastifyInstance) {
   fastify.addHook("preHandler", async (request, reply) => {
-    if (request.routeOptions.url === "/check") return;
+    if (request.url === "/check") return;
     const ip = request.ip;
     const result = bruteShield(ip);
     if (result.blocked) {
       return reply.code(429).send("Too many requests");
     }
   });
-  fastify.get("/register", async () => {
-    return "Hello, World!";
+  fastify.post<{ Body: { email: string; password: string } }>(
+    "/register",
+    async (request) => {
+      const { email, password } = request.body;
+      await registerUser(email, password);
+      return { ok: true };
+    },
+  );
+  fastify.delete("/users", async () => {
+    await query("DELETE FROM users");
+    return { ok: true };
   });
-  fastify.get("/delete", async () => {
-    return true;
-  });
-  fastify.get("/check", async () => {
-    const result = await query("SELECT NOW() as time");
-    return { ok: true, time: result.rows[0].time };
+  fastify.get("/users", async () => {
+    const result = await query("SELECT * FROM users");
+    return result.rows;
   });
 }
