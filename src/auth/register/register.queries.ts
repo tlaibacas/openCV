8c150bce17c4395b28c1bcce9d2bd3129383a609
tlaibacas <tiago.abreu.laibacas@gmail.com>
2026-05-02 15:28:54 +0100

    Removed logs

--- src/auth/register/register.queries.ts
+++ src/auth/register/register.queries.ts
@@ -17,17 +17,6 @@ export const registerUser = async (
     sex ?? null,
   ];
 
-  console.log("REGISTER USER PAYLOAD:", {
-    email,
-    password,
-    name,
-    role,
-    agency,
-    sex,
-  });
-
-  console.log("FINAL SQL VALUES:", payload);
-
   return query(
     "INSERT INTO users (email, password, name, role, agency, sex) VALUES ($1, $2, $3, $4, $5, $6)",
     payload,
