73a1f7970776422dceaf0324c896ff17f23432e6
tlaibacas <tiago.abreu.laibacas@gmail.com>
2026-05-11 11:27:36 +0100

    Fixing up the clean mjs

--- build.mjs
+++ build.mjs
@@ -1,22 +1,4 @@
 import { build } from "esbuild";
-import fs from "fs";
-
-const start = [
-  "dist",
-  "LICENSE",
-  "users.sql",
-  "package-lock.json",
-  "README.md",
-];
-
-const finish = ["src", "esling.config.js", "tsconfig.json", ".gitignore"];
-
-for (const target of start) {
-  fs.rmSync(target, {
-    recursive: true,
-    force: true,
-  });
-}
 
 await build({
   entryPoints: ["src/index.ts"],
@@ -29,10 +11,3 @@ await build({
   minify: true,
   external: ["fastify", "dotenv", "pg"],
 });
-
-for (const target of finish) {
-  fs.rmSync(target, {
-    recursive: true,
-    force: true,
-  });
-}
