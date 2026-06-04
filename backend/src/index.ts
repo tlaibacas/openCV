8eab48060049ce91f64dbc0afc6851ea08bd582a
tlaibacas <tiago.abreu.laibacas@gmail.com>
2026-06-04 12:31:44 +0100

    Fix import and variable name for server initialization in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,11 @@
 // Comments only made to separate functions from types and variables for better readability. They're removed on build.
 
-import { buildsv } from "./server/server.js";
+import { buildServer } from "./server/server.js";
 import { dataCleaner } from "./middleware/bruteShield.js";
 
 dataCleaner();
 
-const server = buildsv();
+const server = buildServer();
 server.listen({ port: 1111, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.error(err);
