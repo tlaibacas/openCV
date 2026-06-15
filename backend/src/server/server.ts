c8f8a0a281ee4a9ef3a274989f5b366e0b52654a
tlaibacas <tiago.abreu.laibacas@gmail.com>
2026-06-15 22:31:09 +0100

    - server has now rate limiter lib - Renamed and unified register and login into authRoutes

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,12 +1,15 @@
 import fastify from "fastify";
-import { rootRoutes } from "../routes/root.routes.js";
-import { registerRoutes } from "../routes/register.routes.js";
-import { loginRoutes } from "../routes/login.routes.js";
+import { rootRoutes } from "../routes/rootRoutes.js";
+import { authRoutes } from "../routes/authRoutes.js";
+import rateLimit from "@fastify/rate-limit";
 
 export function buildServer() {
   const app = fastify();
+  app.register(rateLimit, {
+    max: 120,
+    timeWindow: "1 minute",
+  });
   app.register(rootRoutes, { prefix: "api/v1" });
-  app.register(registerRoutes, { prefix: "api/auth/v1" });
-  app.register(loginRoutes, { prefix: "api/auth/v1" });
+  app.register(authRoutes, { prefix: "api/auth/v1" });
   return app;
 }
