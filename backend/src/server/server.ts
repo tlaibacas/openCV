f40969ff3f91fb4816db4652025a93c200c14e94
tlaibacas <tiago.abreu.laibacas@gmail.com>
2026-06-15 21:42:37 +0100

    -Started to work on login

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,10 +1,12 @@
 import fastify from "fastify";
 import { rootRoutes } from "../routes/root.routes.js";
 import { registerRoutes } from "../routes/register.routes.js";
+import { loginRoutes } from "../routes/login.routes.js";
 
 export function buildServer() {
   const app = fastify();
-  app.register(rootRoutes, { prefix: "api" });
-  app.register(registerRoutes, { prefix: "auth" });
+  app.register(rootRoutes, { prefix: "api/v1" });
+  app.register(registerRoutes, { prefix: "api/auth/v1" });
+  app.register(loginRoutes, { prefix: "api/auth/v1" });
   return app;
 }
