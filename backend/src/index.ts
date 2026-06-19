068b1533ee00dc2d250eb95133997bac2aac505a
tlaibacas <tiago.abreu.laibacas@gmail.com>
2026-06-19 16:19:16 +0100

    -Removed data cleaner

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,6 @@
 // Comments only made to separate functions from types and variables for better readability. They're removed on build.
 
 import { buildServer } from "./server/server.js";
-import { dataCleaner } from "./middleware/bruteShield.js";
-
-dataCleaner();
 
 const server = buildServer();
 server.listen({ port: 1111, host: "0.0.0.0" }, (err, address) => {
