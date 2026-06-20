a5e4faf07b42244feb11b5fb00b743c85159c266
tlaibacas <tiago.abreu.laibacas@gmail.com>
2026-06-20 10:14:21 +0100

    -Better functions

--- backend/src/auth/register/register.services.ts
+++ backend/src/auth/register/register.services.ts
@@ -1,7 +1,7 @@
 import { registerSchema } from "./register.schema.js";
 import { prisma } from "../../lib/prisma.js";
 import * as argon2 from "argon2";
-import type { ArrayResult, Register, Result } from "../../types.js";
+import type { ArrayResult, Register, Result, UpdateUser } from "../../types.js";
 
 const uuidRegex: RegExp =
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
@@ -28,39 +28,31 @@ export const checkId = async (id: string): Promise<Result> =>
           }),
         );
 
-export const register = async (data: Register): Promise<Result> =>
-  (async () => {
-    const parsed = registerSchema.safeParse(data);
+export const register = async (data: Register): Promise<Result> => {
+  const parsed = registerSchema.safeParse(data);
 
-    return !parsed.success
-      ? { success: false, error: parsed.error.issues[0] }
-      : await (async () => {
-          const existingUser = await prisma.user.findUnique({
-            where: { email: parsed.data.email },
-            select: { email: true },
-          });
-
-          return existingUser
-            ? { success: false, error: "Email already exists" }
-            : await (async () => {
-                const hash = await argon2.hash(parsed.data.password);
-
-                const user = await prisma.user.create({
-                  data: { ...parsed.data, password: hash },
-                });
-
-                return {
-                  success: true,
-                  user: {
-                    id: user.id,
-                    name: user.name,
-                    email: user.email,
-                    role: user.role,
-                  },
-                };
-              })();
-        })();
-  })();
+  return !parsed.success
+    ? {
+        success: false,
+        error: parsed.error.issues[0]?.message ?? "Validation error",
+      }
+    : (await prisma.user.findUnique({
+          where: { email: parsed.data.email },
+          select: { email: true },
+        }))
+      ? { success: false, error: "Email already exists" }
+      : {
+          success: true,
+          user: {
+            ...(await prisma.user.create({
+              data: {
+                ...parsed.data,
+                password: await argon2.hash(parsed.data.password),
+              },
+            })),
+          },
+        };
+};
 
 export const users = async (): Promise<ArrayResult> => {
   const users = await prisma.user.findMany({
@@ -84,18 +76,26 @@ export const deleteUser = async (id: string): Promise<Result> => {
     : { success: true, user: await prisma.user.delete({ where: { id } }) };
 };
 
-export async function updateUser(id: string, data: unknown) {
+export const updateUser = async (
+  id: string,
+  data: UpdateUser,
+): Promise<Result> => {
   const idCheck = await checkId(id);
-  if (!idCheck.success) {
-    return idCheck;
-  }
-  return {
-    success: idCheck.success,
-    data,
-    message: "User updated",
-  };
-  // TODO
-}
+
+  return idCheck.success
+    ? {
+        success: true,
+        user: await prisma.user.update({
+          where: { id },
+          data,
+        }),
+        message: "User updated",
+      }
+    : {
+        success: false,
+        error: idCheck.error,
+      };
+};
 
 // TO DELETE!!!!
 export async function checkAll() {
